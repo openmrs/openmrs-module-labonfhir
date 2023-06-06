76370db271d39dfff859c4ad418b9e3a59f2d94c
Mutesasira Moses <mozzymutesa@gmail.com>
2023-06-06 17:54:33 +0300

    format (#33)

--- omod/src/main/java/org/openmrs/module/labonfhir/web/resources/FailedTaskResource.java
+++ omod/src/main/java/org/openmrs/module/labonfhir/web/resources/FailedTaskResource.java
@@ -18,69 +18,68 @@ import org.openmrs.module.webservices.rest.web.annotation.PropertyGetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 
 @Resource(name = RestConstants.VERSION_1 + "/failedtask", supportedClass = FailedTask.class, supportedOpenmrsVersions = {
-    "2.*", "3.*"})
-public class FailedTaskResource extends DataDelegatingCrudResource<FailedTask>{
- 
+        "2.*", "3.*" })
+public class FailedTaskResource extends DataDelegatingCrudResource<FailedTask> {
+    
     LabOnFhirService labOnFhirService = Context.getService(LabOnFhirService.class);
-
+    
     @Override
     public FailedTask newDelegate() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'newDelegate'");
     }
-
+    
     @Override
     public FailedTask save(FailedTask delegate) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'save'");
     }
-
+    
     @Override
     public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
-            DelegatingResourceDescription description = new DelegatingResourceDescription();
-            description.addProperty("display");
-			description.addProperty("taskUuid");
-			description.addProperty("error");
-			description.addProperty("isSent");
-			description.addProperty("uuid");
-			description.addSelfLink();
-			return description;  
+        DelegatingResourceDescription description = new DelegatingResourceDescription();
+        description.addProperty("display");
+        description.addProperty("taskUuid");
+        description.addProperty("error");
+        description.addProperty("isSent");
+        description.addProperty("uuid");
+        description.addSelfLink();
+        return description;
     }
-
+    
     @PropertyGetter("display")
-	public String getDisplayString(FailedTask delegate) {
-
-		return "Task Uuid : " + delegate.getTaskUuid() + " Is Sent : " + delegate.getIsSent();
-	}
+    public String getDisplayString(FailedTask delegate) {
+        return "Task Uuid : " + delegate.getTaskUuid() + " Is Sent : " + delegate.getIsSent();
+    }
     
     @Override
     public FailedTask getByUniqueId(String uniqueId) {
-       return labOnFhirService.getFailedTaskByUuid(uniqueId);
+        return labOnFhirService.getFailedTaskByUuid(uniqueId);
     }
-
+    
     @Override
     protected void delete(FailedTask delegate, String reason, RequestContext context) throws ResponseException {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'delete'");
     }
-
+    
     @Override
     public void purge(FailedTask delegate, RequestContext context) throws ResponseException {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'purge'");
     }
-
+    
     @Override
-	protected NeedsPaging<FailedTask> doSearch(RequestContext context) {
+    protected NeedsPaging<FailedTask> doSearch(RequestContext context) {
         Boolean isSent = Boolean.valueOf(context.getParameter("sent"));
         List<FailedTask> tasks = labOnFhirService.getAllFailedTasks(isSent);
-		return new NeedsPaging<FailedTask>(tasks, context);
-	}
-
-	@Override
-	public PageableResult doGetAll(RequestContext context) {
-		List<FailedTask> tasks = labOnFhirService.getAllFailedTasks(null);
-		return new NeedsPaging<FailedTask>(tasks, context);
-	}
+        return new NeedsPaging<FailedTask>(tasks, context);
+    }
+    
+    @Override
+    public PageableResult doGetAll(RequestContext context) {
+        List<FailedTask> tasks = labOnFhirService.getAllFailedTasks(null);
+        return new NeedsPaging<FailedTask>(tasks, context);
+    }
     
 }
